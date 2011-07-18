bdc03b12502a6ff7ad8ca95c42e6b46391e63d07
Reece Dunn <msclrhd@gmail.com>
2011-07-18 15:02:16 +0100

    cainteoir-gtk: split out the document event processing to a separate source file

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,7 @@ namespace rql = cainteoir::rdf::query;
 namespace tts = cainteoir::tts;
 
 #include "settings.hpp"
-#include "toc.hpp"
+#include "document.hpp"
 #include "voice_selection.hpp"
 
 std::string get_user_file(const char * filename)
@@ -83,58 +83,6 @@ void create_recent_filter(Gtk::RecentFilter & filter, const rdf::graph & aMetada
 			filter.add_mime_type(rql::value(*mimetype));
 	}
 }
-
-struct document : public cainteoir::document_events
-{
-	document()
-		: tts(m_metadata)
-		, m_doc(new cainteoir::document())
-	{
-	}
-
-	void metadata(const std::tr1::shared_ptr<const rdf::triple> &aStatement)
-	{
-		m_metadata.push_back(aStatement);
-	}
-
-	const rdf::bnode genid()
-	{
-		return m_metadata.genid();
-	}
-
-	void text(std::tr1::shared_ptr<cainteoir::buffer> aText)
-	{
-		m_doc->add(aText);
-	}
-
-	void toc_entry(int depth, const rdf::uri &location, const std::string &title)
-	{
-		toc.add(depth, location, title);
-	}
-
-	void anchor(const rdf::uri &location)
-	{
-		m_doc->add_anchor(location);
-	}
-
-	void clear()
-	{
-		m_doc->clear();
-		subject.reset();
-		toc.clear();
-	}
-
-	cainteoir::document::range_type selection() const
-	{
-		return m_doc->children(toc.selection());
-	}
-
-	std::tr1::shared_ptr<const rdf::uri> subject;
-	rdf::graph m_metadata;
-	cainteoir::tts::engines tts;
-	std::tr1::shared_ptr<cainteoir::document> m_doc;
-	TocPane toc;
-};
 
 class MetadataView : public Gtk::VBox
 {

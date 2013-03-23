3c18aa13b3e70462a74ae0527b3bc01c07a2f0b4
Reece H. Dunn <msclrhd@gmail.com>
2013-03-23 15:02:21 +0000

    autotools: require nullptr support

--- src/compatibility.hpp
+++ src/compatibility.hpp
@@ -1,6 +1,6 @@
 /* Language and System compatibility helpers.
  *
- * Copyright (C) 2011-2012 Reece H. Dunn
+ * Copyright (C) 2011-2013 Reece H. Dunn
  *
  * This file is part of cainteoir-gtk.
  *
@@ -25,45 +25,6 @@
 
 #if !GTK_CHECK_VERSION(3, 3, 0)
 void gtk_window_set_hide_titlebar_when_maximized(GtkWindow *, gboolean);
-#endif
-
-#ifndef HAVE_CXX11_NULLPTR
-	const struct nullptr_t 
-	{
-		template <typename T>
-		inline operator T *() const { return 0; }
-
-		template <typename C, typename T>
-		inline operator T C:: *() const { return 0; }
-	private:
-		void operator&() const;
-	} nullptr = {};
-
-#ifndef HAVE_CXX11_IMPLICIT_NULLPTR_COMPARE
-	template <typename T>
-	inline bool operator==(const T &a, const nullptr_t &b)
-	{
-		return a == NULL;
-	}
-
-	template <typename T>
-	inline bool operator==(const nullptr_t &a, const T &b)
-	{
-		return b == NULL;
-	}
-
-	template <typename T>
-	inline bool operator!=(const T &a, const nullptr_t &b)
-	{
-		return a != NULL;
-	}
-
-	template <typename T>
-	inline bool operator!=(const nullptr_t &a, const T &b)
-	{
-		return b != NULL;
-	}
-#endif
 #endif
 
 #ifndef HAVE_CXX11_STD_BEGIN

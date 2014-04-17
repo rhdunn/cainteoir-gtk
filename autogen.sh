8c38a292ab92ef994eaecbdc45823700409b23a5
Reece H. Dunn <msclrhd@gmail.com>
2014-04-17 11:00:03 +0100

    autotools: use glibtoolize on Mac OSX

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+LIBTOOLIZE=`which libtoolize`
+if ! test -f "$LIBTOOLIZE" ; then
+	# Mac OSX support ...
+	LIBTOOLIZE=`which glibtoolize`
+fi
+
 mkdir -p m4
 ln -svf README.md README
 ln -svf docs/ReleaseNotes.md NEWS
@@ -12,7 +18,7 @@ run()
 
 run glib-gettextize --force --copy
 run intltoolize --automake --copy --force
-run libtoolize --copy --force
+run ${LIBTOOLIZE} --copy --force
 run aclocal -I m4
 run autoheader
 run automake --add-missing

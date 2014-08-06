b74b8df3d8fb7bba3af6b894b0595a0bd7a02b0f
Reece H. Dunn <msclrhd@gmail.com>
2014-08-06 21:42:44 +0100

    cainteoir_document_view: implement GtkScrollable

--- src/libcainteoir-gtk/cainteoir_document_view.cpp
+++ src/libcainteoir-gtk/cainteoir_document_view.cpp
@@ -35,18 +35,41 @@ struct _CainteoirDocumentViewPrivate
 enum
 {
 	PROP_0,
+	// GtkScrollable interface:
+	PROP_HADJUSTMENT,
+	PROP_VADJUSTMENT,
+	PROP_HSCROLL_POLICY,
+	PROP_VSCROLL_POLICY,
 };
 
-G_DEFINE_TYPE_WITH_PRIVATE(CainteoirDocumentView, cainteoir_document_view, GTK_TYPE_BIN)
+G_DEFINE_TYPE_WITH_CODE(CainteoirDocumentView, cainteoir_document_view, GTK_TYPE_BIN,
+	G_IMPLEMENT_INTERFACE(GTK_TYPE_SCROLLABLE, nullptr))
+
+#define CAINTEOIR_DOCUMENT_VIEW_GET_PRIVATE(obj) \
+	(G_TYPE_INSTANCE_GET_PRIVATE((obj), CAINTEOIR_TYPE_DOCUMENT_VIEW, CainteoirDocumentViewPrivate))
 
 static void
 cainteoir_document_view_set_property(GObject *object, guint prop_id, const GValue *value, GParamSpec *pspec)
 {
 	CainteoirDocumentView *view = CAINTEOIR_DOCUMENT_VIEW(object);
+	GtkScrollable *scroll = GTK_SCROLLABLE(view->priv->text_view);
 	switch (prop_id)
 	{
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+		break;
+	// GtkScrollable interface:
+	case PROP_HADJUSTMENT:
+		gtk_scrollable_set_hadjustment(scroll, GTK_ADJUSTMENT(g_value_get_object(value)));
+		break;
+	case PROP_VADJUSTMENT:
+		gtk_scrollable_set_vadjustment(scroll, GTK_ADJUSTMENT(g_value_get_object(value)));
+		break;
+	case PROP_HSCROLL_POLICY:
+		gtk_scrollable_set_hscroll_policy(scroll, (GtkScrollablePolicy)g_value_get_enum(value));
+		break;
+	case PROP_VSCROLL_POLICY:
+		gtk_scrollable_set_vscroll_policy(scroll, (GtkScrollablePolicy)g_value_get_enum(value));
 		break;
 	}
 }
@@ -55,10 +78,24 @@ static void
 cainteoir_document_view_get_property(GObject *object, guint prop_id, GValue *value, GParamSpec *pspec)
 {
 	CainteoirDocumentView *view = CAINTEOIR_DOCUMENT_VIEW(object);
+	GtkScrollable *scroll = GTK_SCROLLABLE(view->priv->text_view);
 	switch (prop_id)
 	{
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+		break;
+	// GtkScrollable interface:
+	case PROP_HADJUSTMENT:
+		g_value_set_object(value, gtk_scrollable_get_hadjustment(scroll));
+		break;
+	case PROP_VADJUSTMENT:
+		g_value_set_object(value, gtk_scrollable_get_vadjustment(scroll));
+		break;
+	case PROP_HSCROLL_POLICY:
+		g_value_set_enum(value, gtk_scrollable_get_hscroll_policy(scroll));
+		break;
+	case PROP_VSCROLL_POLICY:
+		g_value_set_enum(value, gtk_scrollable_get_vscroll_policy(scroll));
 		break;
 	}
 }
@@ -79,12 +116,20 @@ cainteoir_document_view_class_init(CainteoirDocumentViewClass *klass)
 	object->set_property = cainteoir_document_view_set_property;
 	object->get_property = cainteoir_document_view_get_property;
 	object->finalize = cainteoir_document_view_finalize;
+
+	g_type_class_add_private(object, sizeof(CainteoirDocumentViewPrivate));
+
+	// GtkScrollable interface:
+	g_object_class_override_property(object, PROP_HADJUSTMENT,    "hadjustment");
+	g_object_class_override_property(object, PROP_VADJUSTMENT,    "vadjustment");
+	g_object_class_override_property(object, PROP_HSCROLL_POLICY, "hscroll-policy");
+	g_object_class_override_property(object, PROP_VSCROLL_POLICY, "vscroll-policy");
 }
 
 static void
 cainteoir_document_view_init(CainteoirDocumentView *view)
 {
-	view->priv = (CainteoirDocumentViewPrivate *)cainteoir_document_view_get_instance_private(view);
+	view->priv = CAINTEOIR_DOCUMENT_VIEW_GET_PRIVATE(view);
 	view->priv->text_view = gtk_text_view_new();
 	view->priv->doc = nullptr;
 }
@@ -95,6 +140,7 @@ cainteoir_document_view_new()
 	CainteoirDocumentView *view = CAINTEOIR_DOCUMENT_VIEW(g_object_new(CAINTEOIR_TYPE_DOCUMENT_VIEW, nullptr));
 	gtk_container_add(GTK_CONTAINER(view), view->priv->text_view);
 	gtk_text_view_set_editable(GTK_TEXT_VIEW(view->priv->text_view), FALSE);
+	gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(view->priv->text_view), GTK_WRAP_WORD);
 	return GTK_WIDGET(view);
 }
 

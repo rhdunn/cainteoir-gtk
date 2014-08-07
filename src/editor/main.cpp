28457c927dd5ddaed258a4d0aaac43fc62abf307
Reece H. Dunn <msclrhd@gmail.com>
2014-08-07 12:13:37 +0100

    Create a simple editor application.

---
+++ src/editor/main.cpp
@@ -0,0 +1,48 @@
+/* Cainteoir Editor Application.
+ *
+ * Copyright (C) 2014 Reece H. Dunn
+ *
+ * This file is part of cainteoir-gtk.
+ *
+ * cainteoir-gtk is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cainteoir-gtk is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cainteoir-gtk.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+#include "i18n.h"
+
+#include <gtk/gtk.h>
+
+static void
+on_window_destroy(GtkWidget *object, gpointer data)
+{
+	gtk_main_quit();
+}
+
+int
+main(int argc, char ** argv)
+{
+	gtk_init(&argc, &argv);
+
+	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
+
+	gtk_window_set_title(GTK_WINDOW(window), i18n("Cainteoir Text-to-Speech Editor"));
+
+	g_signal_connect(window, "destroy", G_CALLBACK(on_window_destroy), nullptr);
+
+	gtk_widget_show_all(window);
+	gtk_main();
+
+	return 0;
+}

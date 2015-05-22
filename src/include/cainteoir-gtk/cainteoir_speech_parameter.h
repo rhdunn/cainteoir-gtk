1657801651bc69fca0fef34763e6c438c416455b
Reece H. Dunn <msclrhd@gmail.com>
2015-05-22 18:34:03 +0000

    Create a CainteoirSpeechParameter class.

---
+++ src/include/cainteoir-gtk/cainteoir_speech_parameter.h
@@ -0,0 +1,66 @@
+/* A GTK+ wrapper around the cainteoir::tts::parameter class.
+ *
+ * Copyright (C) 2015 Reece H. Dunn
+ *
+ * This file is part of cainteoir-gtk.
+ *
+ * cainteoir-gtk is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cainteoir-gtk is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cainteoir-gtk.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CAINTEOIR_GTK_CAINTEOIR_SPEECH_PARAMETER_H
+#define CAINTEOIR_GTK_CAINTEOIR_SPEECH_PARAMETER_H
+
+G_BEGIN_DECLS
+
+#define CAINTEOIR_TYPE_SPEECH_PARAMETER \
+	(cainteoir_speech_parameter_get_type())
+#define CAINTEOIR_SPEECH_PARAMETER(obj) \
+	(G_TYPE_CHECK_INSTANCE_CAST((obj), CAINTEOIR_TYPE_SPEECH_PARAMETER, CainteoirSpeechParameter))
+#define CAINTEOIR_SPEECH_PARAMETER_CLASS(klass) \
+	(G_TYPE_CHECK_CLASS_CAST((klass), CAINTEOIR_TYPE_SPEECH_PARAMETER, CainteoirSpeechParameterClass))
+#define CAINTEOIR_IS_SPEECH_PARAMETER(obj) \
+	(G_TYPE_CHECK_INSTANCE_TYPE((obj), CAINTEOIR_TYPE_SPEECH_PARAMETER))
+#define CAINTEOIR_IS_SPEECH_PARAMETER_CLASS(klass) \
+	(G_TYPE_CHECK_CLASS_TYPE((klass), CAINTEOIR_TYPE_SPEECH_PARAMETER))
+#define CAINTEOIR_SPEECH_PARAMETER_GET_CLASS(obj) \
+	(G_TYPE_INSTANCE_GET_CLASS((obj), CAINTEOIR_TYPE_SPEECH_PARAMETER, CainteoirSpeechParameterClass))
+
+typedef struct _CainteoirSpeechParameter        CainteoirSpeechParameter;
+typedef struct _CainteoirSpeechParameterPrivate CainteoirSpeechParameterPrivate;
+typedef struct _CainteoirSpeechParameterClass   CainteoirSpeechParameterClass;
+
+struct _CainteoirSpeechParameter
+{
+	GObject parent_instance;
+
+	/*< private >*/
+	CainteoirSpeechParameterPrivate *priv;
+};
+
+struct _CainteoirSpeechParameterClass
+{
+	GObjectClass parent_class;
+
+	/* Padding for future expansion */
+	void (*_reserved1)(void);
+	void (*_reserved2)(void);
+	void (*_reserved3)(void);
+	void (*_reserved4)(void);
+};
+
+GType                                  cainteoir_speech_parameter_get_type(void) G_GNUC_CONST;
+
+G_END_DECLS
+
+#endif
---
+++ src/libcainteoir-gtk/cainteoir_speech_parameter.cpp
@@ -0,0 +1,65 @@
+/* A GTK+ wrapper around the cainteoir::tts::parameter class.
+ *
+ * Copyright (C) 2015 Reece H. Dunn
+ *
+ * This file is part of cainteoir-gtk.
+ *
+ * cainteoir-gtk is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cainteoir-gtk is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cainteoir-gtk.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include <gtk/gtk.h>
+
+#include <cainteoir-gtk/cainteoir_speech_parameter.h>
+
+#include "cainteoir_speech_parameter_private.h"
+
+struct _CainteoirSpeechParameterPrivate
+{
+	std::shared_ptr<cainteoir::tts::parameter> param;
+};
+
+G_DEFINE_TYPE_WITH_PRIVATE(CainteoirSpeechParameter, cainteoir_speech_parameter, G_TYPE_OBJECT)
+
+static void
+cainteoir_speech_parameter_finalize(GObject *object)
+{
+	CainteoirSpeechParameter *param = CAINTEOIR_SPEECH_PARAMETER(object);
+	param->priv->~CainteoirSpeechParameterPrivate();
+
+	G_OBJECT_CLASS(cainteoir_speech_parameter_parent_class)->finalize(object);
+}
+
+static void
+cainteoir_speech_parameter_class_init(CainteoirSpeechParameterClass *klass)
+{
+	GObjectClass *object = G_OBJECT_CLASS(klass);
+	object->finalize = cainteoir_speech_parameter_finalize;
+}
+
+static void
+cainteoir_speech_parameter_init(CainteoirSpeechParameter *param)
+{
+	void * data = cainteoir_speech_parameter_get_instance_private(param);
+	param->priv = new (data)CainteoirSpeechParameterPrivate();
+}
+
+CainteoirSpeechParameter *
+cainteoir_speech_parameter_new(std::shared_ptr<cainteoir::tts::parameter> &parameter)
+{
+	CainteoirSpeechParameter *self = CAINTEOIR_SPEECH_PARAMETER(g_object_new(CAINTEOIR_TYPE_SPEECH_PARAMETER, nullptr));
+	self->priv->param = parameter;
+	return self;
+}
